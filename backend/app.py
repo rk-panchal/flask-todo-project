78545c5a22436d055cc55037ce9d1ab8a962b7af
rk-panchal <panchalrk89@gmail.com>
2026-05-14 14:11:01 +0530

    Store data in mongo and load creds from env

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify # we also import 'render_template' to render HTML templates
+from dotenv import load_dotenv # we import 'load_dotenv' to load environment variables from a .env file
+from urllib.parse import quote_plus
+import os # we import 'os' to access environment variables
+# from pymongo import MongoClient # we import 'MongoClient' to connect to our MongoDB
+import pymongo # we import 'pymongo' to connect to our MongoDB database and perform database operations
+from pymongo import MongoClient
+from bson import ObjectId
+
+load_dotenv() # we call 'load_dotenv()' to load environment variables from the .env file
+
+username = quote_plus(os.getenv("MONGO_USER"))
+password = quote_plus(os.getenv("MONGO_PASS"))
+
+MONGO_URI = f"mongodb+srv://{username}:{password}@rk.9hyvp36.mongodb.net/?appName=RK"
+
+client = MongoClient(MONGO_URI)
+
+db = client["flask-todo-project"]   # database
+collection = db["flask-todo-project-collection"]  # collection
 
 app = Flask(__name__)
 @app.route('/submittodoitem', methods=["POST"])
@@ -10,4 +29,4 @@ def submittodoitem():
     return form_data
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+        app.run(host='0.0.0.0', port=1000, debug=True) 
